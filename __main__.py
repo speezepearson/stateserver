a1f27b98893c24911b0b0c7027f65627cc235620
Spencer Pearson <suspense@cs.washington.edu>
2019-07-06 08:20:16 -0700

    refactor out Resource class; add --state-dir CLI arg

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import collections
+import dataclasses
 import json
 import pprint
 from pathlib import Path
@@ -9,8 +10,22 @@ from typing import Mapping
 from aiohttp import web
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--state-dir', type=Path, default=Path.cwd())
 parser.add_argument('-p', '--port', type=int, default=8002)
 args = parser.parse_args()
+
+@dataclasses.dataclass
+class Resource:
+    path: Path
+
+    def get(self):
+        if self.path.is_file():
+            return json.load(self.path.open())
+        else:
+            return None
+
+    def put(self, j):
+        json.dump(j, self.path.open('w'))
 
 routes = web.RouteTableDef()
 conditions: Mapping[str, asyncio.Condition] = collections.defaultdict(asyncio.Condition)
@@ -18,43 +33,41 @@ conditions: Mapping[str, asyncio.Condition] = collections.defaultdict(asyncio.Co
 def get_state_path(name: str) -> Path:
     if not name.isalnum():
         raise ValueError(f'non alphanumeric: {name!r}')
-    return (Path('states')/name).with_suffix('.json')
-
-def get_state(name: str):
-    path = get_state_path(name)
-    if path.is_file():
-        return json.load(path.open())
-    else:
-        return None
-
-routes.static(prefix='/static', path=Path.cwd()/'static', follow_symlinks=True)
+    return (args.state_dir/name).with_suffix('.json')
 
 @routes.route('GET', '/states/{name:[a-zA-Z0-9]{,32}}')
 async def server_get_state(request: web.Request):
     name: str = request.match_info['name']
-    print('GET /states/' + name)
     assert name.isalnum()
+    resource = Resource(get_state_path(name))
+    print('GET /states/' + name)
     async with conditions[name]:
-        return web.json_response({"current_state": get_state(name)})
+        return web.json_response({"current_state": resource.get()})
 
 @routes.route('POST', '/poll/{name:[a-zA-Z0-9]{,32}}')
 async def server_poll(request: web.Request):
     name: str = request.match_info['name']
-    print('POST /poll/' + name)
     assert name.isalnum()
+    resource = Resource(get_state_path(name))
+    print('POST /poll/' + name)
 
-    old_state = (await request.json())['current_state']
+    try:
+        old_state = (await request.json())['current_state']
+    except ValueError:
+        return web.Response(body='syntactically invalid JSON request', status=400)
+    except (TypeError, KeyError) as e:
+        return web.Response(body=f'semantically invalid JSON request: {e}', status=400)
     condition = conditions[name]
     async with condition:
-        await condition.wait_for(lambda: get_state(name) != old_state)
-        return web.json_response({"current_state": get_state(name)})
+        await condition.wait_for(lambda: resource.get() != old_state)
+        return web.json_response({"current_state": resource.get()})
 
 @routes.route('POST', '/states/{name:[a-zA-Z0-9]{,32}}')
 async def server_post_state(request: web.Request):
     name: str = request.match_info['name']
-    print('POST /states/' + name)
     assert name.isalnum()
-    path = get_state_path(name)
+    resource = Resource(get_state_path(name))
+    print('POST /states/' + name)
     try:
         request_state = await request.json()
     except ValueError:
@@ -65,11 +78,11 @@ async def server_post_state(request: web.Request):
     request_new = request_state['new']
 
     async with conditions[name]:
-        current = get_state(name)
+        current = resource.get()
         if current == request_old:
             print("transitioning:")
             pprint.pprint(request_state)
-            path.write_text(json.dumps(request_new))
+            resource.put(request_new)
             conditions[name].notify_all()
             return web.json_response({"success": True, "current_state": request_new})
         else:
