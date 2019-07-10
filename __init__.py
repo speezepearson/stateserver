16e3d6313c6711732466ad60004e441b8b9c48f3
Spencer Pearson <spencerpearson@dropbox.com>
2019-07-09 21:50:59 -0700

    make usable as library; change paths to `/{name}` and `/{name}/poll`

---
+++ __init__.py
@@ -0,0 +1,80 @@
+import argparse
+import asyncio
+import collections
+import json
+
+from functools import partial
+from pathlib import Path
+from typing import Iterable, Mapping, NamedTuple, NewType
+
+from aiohttp import web
+
+ResourceName = NewType('ResourceName', str)
+
+class Resource(NamedTuple): # would like to use dataclasses, but installing 3.7 is nontrivial
+    path: Path
+
+    def get(self):
+        if self.path.is_file():
+            return json.load(self.path.open())
+        else:
+            return None
+
+    def put(self, j):
+        json.dump(j, self.path.open('w'))
+
+def make_routes(state_dir: Path) -> Iterable[web.RouteDef]:
+    conditions: Mapping[ResourceName, asyncio.Condition] = collections.defaultdict(asyncio.Condition)
+    for method, path, func in [('GET', '/{name:[a-zA-Z0-9]{,32}}', get_state_response),
+                               ('POST', '/{name:[a-zA-Z0-9]{,32}}/poll', poll_response),
+                               ('POST', '/{name:[a-zA-Z0-9]{,32}}', post_state_response),
+                               ]:
+        yield web.route(method, path, partial(func, state_dir=state_dir, conditions=conditions))
+
+
+def get_state_path(state_dir: Path, name: ResourceName) -> Path:
+    if not name.isalnum():
+        raise ValueError(f'non alphanumeric: {name!r}')
+    return (state_dir/name).with_suffix('.json')
+
+async def get_state_response(request: web.Request, state_dir: Path, conditions: Mapping[ResourceName, asyncio.Condition]):
+    name: ResourceName = request.match_info['name']
+    resource = Resource(get_state_path(state_dir, name))
+    async with conditions[name]:
+        return web.json_response({"current_state": resource.get()})
+
+async def poll_response(request: web.Request, state_dir: Path, conditions: Mapping[ResourceName, asyncio.Condition]):
+    name: ResourceName = request.match_info['name']
+    resource = Resource(get_state_path(state_dir, name))
+
+    try:
+        old_state = (await request.json())['current_state']
+    except ValueError:
+        return web.Response(body='syntactically invalid JSON request', status=400)
+    except (TypeError, KeyError) as e:
+        return web.Response(body=f'semantically invalid JSON request: {e}', status=400)
+    condition = conditions[name]
+    async with condition:
+        await condition.wait_for(lambda: resource.get() != old_state)
+        return web.json_response({"current_state": resource.get()})
+
+async def post_state_response(request: web.Request, state_dir: Path, conditions: Mapping[ResourceName, asyncio.Condition]):
+    name: ResourceName = request.match_info['name']
+    resource = Resource(get_state_path(state_dir, name))
+    try:
+        request_state = await request.json()
+    except ValueError:
+        return web.Response(body='syntactically invalid JSON request', status=400)
+    if not (isinstance(request_state, dict) and set(request_state.keys()) == {'old', 'new'}):
+        return web.Response(body='semantically invalid JSON request', status=400)
+    request_old = request_state['old']
+    request_new = request_state['new']
+
+    async with conditions[name]:
+        current = resource.get()
+        if current == request_old:
+            resource.put(request_new)
+            conditions[name].notify_all()
+            return web.json_response({"success": True, "current_state": request_new})
+        else:
+            return web.json_response({"success": False, "current_state": current})
--- __main__.py
+++ __main__.py
@@ -1,93 +1,15 @@
 import argparse
-import asyncio
-import collections
-import json
-import pprint
 from pathlib import Path
-from typing import Mapping, NamedTuple
 
 from aiohttp import web
 
+from . import make_routes
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--state-dir', type=Path, default=Path.cwd())
-parser.add_argument('-p', '--port', type=int, default=8002)
+parser.add_argument('-p', '--port', type=int, default=48402)
 args = parser.parse_args()
 
-class Resource(NamedTuple): # would like to use dataclasses, but installing 3.7 is nontrivial
-    path: Path
-
-    def get(self):
-        if self.path.is_file():
-            return json.load(self.path.open())
-        else:
-            return None
-
-    def put(self, j):
-        json.dump(j, self.path.open('w'))
-
-routes = web.RouteTableDef()
-conditions: Mapping[str, asyncio.Condition] = collections.defaultdict(asyncio.Condition)
-
-def get_state_path(name: str) -> Path:
-    if not name.isalnum():
-        raise ValueError(f'non alphanumeric: {name!r}')
-    return (args.state_dir/name).with_suffix('.json')
-
-@routes.route('GET', '/states/{name:[a-zA-Z0-9]{,32}}')
-async def server_get_state(request: web.Request):
-    name: str = request.match_info['name']
-    assert name.isalnum()
-    resource = Resource(get_state_path(name))
-    print('GET /states/' + name)
-    async with conditions[name]:
-        return web.json_response({"current_state": resource.get()})
-
-@routes.route('POST', '/poll/{name:[a-zA-Z0-9]{,32}}')
-async def server_poll(request: web.Request):
-    name: str = request.match_info['name']
-    assert name.isalnum()
-    resource = Resource(get_state_path(name))
-    print('POST /poll/' + name)
-
-    try:
-        old_state = (await request.json())['current_state']
-    except ValueError:
-        return web.Response(body='syntactically invalid JSON request', status=400)
-    except (TypeError, KeyError) as e:
-        return web.Response(body=f'semantically invalid JSON request: {e}', status=400)
-    condition = conditions[name]
-    async with condition:
-        await condition.wait_for(lambda: resource.get() != old_state)
-        return web.json_response({"current_state": resource.get()})
-
-@routes.route('POST', '/states/{name:[a-zA-Z0-9]{,32}}')
-async def server_post_state(request: web.Request):
-    name: str = request.match_info['name']
-    assert name.isalnum()
-    resource = Resource(get_state_path(name))
-    print('POST /states/' + name)
-    try:
-        request_state = await request.json()
-    except ValueError:
-        return web.Response(body='syntactically invalid JSON request', status=400)
-    if not (isinstance(request_state, dict) and set(request_state.keys()) == {'old', 'new'}):
-        return web.Response(body='semantically invalid JSON request', status=400)
-    request_old = request_state['old']
-    request_new = request_state['new']
-
-    async with conditions[name]:
-        current = resource.get()
-        if current == request_old:
-            print("transitioning:")
-            pprint.pprint(request_state)
-            resource.put(request_new)
-            conditions[name].notify_all()
-            return web.json_response({"success": True, "current_state": request_new})
-        else:
-            print("not transitioning")
-            pprint.pprint({"actual": current, "supposed": request_old, "desired": request_new})
-            return web.json_response({"success": False, "current_state": current})
-
 app = web.Application()
-app.add_routes(routes)
+app.add_routes(make_routes(state_dir=args.state_dir))
 web.run_app(app, port=args.port)
